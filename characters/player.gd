36847e03da4c60e9bd125b278c7ee26ffc9cf621
UI369 <UI369x@gmail.com>
2023-04-30 21:27:12 -0700

    add player movement

--- characters/player.gd
+++ characters/player.gd
@@ -43,18 +43,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	move_and_slide()
-	update_animation()
 	update_facing_direction()
-
-func update_animation(): 
-	if not animation_locked:
-		if not is_on_floor():
-			animated_sprite.play("jump_loop")
-		else:
-			if direction.x != 0:
-				animated_sprite.play("run")
-			else: 
-				animated_sprite.play("idle")
 
 func update_facing_direction():
 	if direction.x > 0:
