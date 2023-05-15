3e0e82786f25f3a4c4e0535d021168d62929275c
UI369 <UI369x@gmail.com>
2023-05-14 21:22:31 -0700

    change hilite to use shaders

--- world/game_element.gd
+++ world/game_element.gd
@@ -6,6 +6,8 @@ class_name GameElement
 
 @export_enum ("bed", "computer", "couch") var id = 0
 
+var outline_material : Material;
+
 signal active_element_signal(element)
 signal inactive_element_signal(element)
 signal game_element_entered(element)
@@ -15,6 +17,8 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	$element_sprite.texture = texture
+	outline_material = $element_sprite.material;
+	$element_sprite.material = null;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,17 +26,18 @@ func _process(delta):
 
 func _on_body_entered(body):
 	print("body entered")
-	$element_sprite.texture = texture_hilite;
+	$element_sprite.material = outline_material;
+	#$element_sprite.texture = texture_hilite;
 	active_element_signal.emit(self)
 
 func _on_body_exited(body):
 	print("body exited")
-	$element_sprite.texture = texture;
+	$element_sprite.material = null;
+	#$element_sprite.texture = texture;
 	inactive_element_signal.emit(self)
 	
 func _do_active_action(tek, action):
 	action.call()
-
 
 func _on_active_element_signal(element):
 	#_do_active_action()
